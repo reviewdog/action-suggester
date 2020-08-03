70199ef0406a30f74446a1de9904c2adc2e3388e
haya14busa <haya14busa@gmail.com>
2020-08-03 09:19:11 +0000

    add test data for sh

---
+++ testdata/test.sh
@@ -0,0 +1,7 @@
+find . -name *.ogg                # Unquoted find/grep patterns
+
+grep '*foo*' file                 # Globs in regex contexts
+
+var = 42                          # Spaces around = in assignments
+
+echo "The time is `date`"         # Use $() instead
