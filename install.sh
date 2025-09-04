51237dd1b6ad64a02acf805c863a0c6c4c72b17e
iokira <iam.iokiller@gmail.com>
2025-09-04 16:28:55 +0900

    Revert "Auto updates by the depup workflow"
    This reverts commit b2103ea045c0021013deb89957ebb494f91199f7.

--- install.sh
+++ install.sh
@@ -13,16 +13,25 @@ if [ -z "${TEMP}" ]; then
   fi
 fi
 
-INSTALL_SCRIPT="$GITHUB_ACTION_PATH/install-reviewdog.sh"
+INSTALL_SCRIPT='https://raw.githubusercontent.com/reviewdog/reviewdog/fd59714416d6d9a1c0692d872e38e7f8448df4fc/install.sh'
 if [ "${VERSION}" = 'nightly' ]; then
-  INSTALL_SCRIPT="$GITHUB_ACTION_PATH/install-nightly.sh"
+  INSTALL_SCRIPT='https://raw.githubusercontent.com/reviewdog/nightly/30fccfe9f47f7e6fd8b3c38aa0da11a6c9f04de7/install.sh'
   VERSION='latest'
 fi
 
 mkdir -p "${TEMP}/reviewdog/bin"
 
 echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
-cat "${INSTALL_SCRIPT}" | sh -s -- -b "${TEMP}/reviewdog/bin" "${VERSION}" 2>&1
+(
+  if command -v curl 2>&1 >/dev/null; then
+    curl -sfL "${INSTALL_SCRIPT}"
+  elif command -v wget 2>&1 >/dev/null; then
+    wget -O - "${INSTALL_SCRIPT}"
+  else
+    echo "curl or wget is required" >&2
+    exit 1
+  fi
+) | sh -s -- -b "${TEMP}/reviewdog/bin" "${VERSION}" 2>&1
 echo '::endgroup::'
 
 echo "${TEMP}/reviewdog/bin" >>"${GITHUB_PATH}"
